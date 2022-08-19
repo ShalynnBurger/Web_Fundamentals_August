9b37f5480667f670b30250d99326a0bca30bba2b
ShalynnBurger <shalynnburger@gmail.com>
2022-08-19 18:13:44 -0500

    fizzbuzz

---
+++ 02-CSS/Fizzbuzz/fizzbuzz.js
@@ -0,0 +1,16 @@
+
+for(let i=1; i<=100; i++){
+    if(i%3 && i%5 ==0) {
+        console.log("FizzBuzz");
+    }
+    if(i%5 ==0) {
+        console.log("Buzz");
+    }
+    if(i%3 ==0){
+        console.log("Fizz");
+    }
+   
+    else {
+    console.log(i);
+  } 
+}
