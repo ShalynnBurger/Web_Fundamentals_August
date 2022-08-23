7039748f576b92d74c7da6daca197e1e9c4372ae
ShalynnBurger <shalynnburger@gmail.com>
2022-08-23 16:24:28 -0500

    Button_clicker

---
+++ 03-week3/Button_clicker/script.js
@@ -0,0 +1,13 @@
+console.log("sanity check")
+
+function likevote(){
+    alert("Ninja was liked")
+}
+
+function uploadDef(btn){
+   btn.remove();
+}
+
+function loginout(inbtn){
+   inbtn.innerText = "Logout"; 
+}
